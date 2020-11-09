d814a7675226ce803ac9552e2461b5ece399c391
Victoria Schorn <victoria.schorn03@htl-salzburg.ac.at>
2020-11-09 14:14:32 +0100

    Add ExampleClass

---
+++ Assets/Scripts/ExampleClass/ExampleClass.cs
@@ -0,0 +1,18 @@
+﻿// Bibliotheken / Asembly Referenz / Namespaces
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// Deklaration einer Klasse
+
+//public            -> access modifier (Zugriffsrechte) / Keyword (vorresavierte Schlüssel...
+//class             -> keyword zur Deklaration einer Klasse
+//ExampleClass      -> identifier (Name der Klasse)
+// : MonoBehaviour  -> Erstellen einer Unterklasse von MonoBehavior
+// {}               -> Scope/Wirkungsbereich
+public class ExampleClass : MonoBehaviour
+{
+
+
+}
